e4f28de5bdc394e5cfa4ba97f86f3180497698e9
Joao Salles <jadsalles@gmail.com>
2019-09-10 00:43:44 -0300

    small changes to create login page

--- social/views.py
+++ social/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+
+def login(request):
+
+    context={}
+    
+    return render(request, 'social/login.html', context)
+
